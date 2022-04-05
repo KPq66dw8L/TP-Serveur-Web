a335598ee6a5ae07d4a5ed9190791a80700f3f29
KPq66dw8L <kezhkia@protonmail.com>
2022-04-05 11:14:19 +0200

    add user with post request now and get params with queryParams AKA the right way, to me

--- Partie1/web-serv-projet/build/resources/main/views/users/users.ftl
+++ Partie1/web-serv-projet/build/resources/main/views/users/users.ftl
@@ -8,6 +8,17 @@
     </#list>
 </ul>
 
+<form method='post' enctype='multipart/form-data'>
+  <input type='text' name='firstname' >
+  <input type='text' name='lastname' >
+  <input type='text' name='prof' >
+  <input type='text' name='blanche' >
+  <input type='text' name='rouge' >
+  <input type='text' name='verte' >
+
+  <button>Upload picture</button>
+</form>
+
 </body>
 
 </html>
--- Partie1/web-serv-projet/src/com/uca/StartServer.java
+++ Partie1/web-serv-projet/src/com/uca/StartServer.java
@@ -5,6 +5,11 @@ import com.uca.dao._Initializer;
 import com.uca.entity.UserEntity;
 import com.uca.gui.*;
 
+import javax.servlet.MultipartConfigElement;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import static java.lang.Integer.parseInt;
 import static spark.Spark.*;
 
@@ -27,29 +32,56 @@ public class StartServer {
             return UserGUI.getAllUsers();
         });
 
-        get("/users/add/:firstname/:lastname/:prof/:blanche/:rouge/:verte", (req, res) -> {
-            //newUser.setId(1);
-//            UserEntity newUser = new UserEntity();
-//            newUser.setFirstName(req.params(":firstname"));
-//            newUser.setLastName(req.params(":lastname"));
-//            newUser.setProf(req.params(":prof"));
-//            newUser.setBlanche(parseInt(req.params(":blanche")));
-//            newUser.setRouge(parseInt(req.params(":rouge")));
-//            newUser.setVerte(parseInt(req.params(":verte")));
-//
-//            newUser = new UserDAO().create(newUser);
-//
-//            return "added " + newUser.getFirstName();
-            
-            res.redirect("/users");
 
-            return UserGUI.create(req.params(":firstname"), req.params(":lastname"), req.params(":prof"), req.params(":blanche"), req.params(":rouge"), req.params(":verte"));
+        /*
+         * ***
+         * L'URL ressemblera a: localhost:8081/users/add?firstname=&lastname=&prof=&blanche=&rouge=&verte=
+         * ***
+         **/
+//        get("/users/add", (req, res) -> {
+//
+//            res.redirect("/users");
+//            return UserGUI.create(req.queryParams("firstname"), req.queryParams("lastname"), req.queryParams("prof"), req.queryParams("blanche"), req.queryParams("rouge"), req.queryParams("verte"));
+//        });
+
+        /*
+         * ***
+         * Pourquoi:
+         * curl -X POST -F 'firstname=test' -F 'lastname=julien' -F 'prof=Mireille' -F 'blanche=0' -F 'rouge=0' -F 'verte=0' http://localhost:8081/users
+         * Ne marche pas?
+         * ***
+         **/
+        post("/users", (req, res) -> {
+
+            String firstname = new String(), lastname = new String(), prof = new String(), blanche = new String(), rouge = new String(), verte =new String();
+            req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp")); // ne sait pas ce que ce truc fait yet
+
+            /*
+             * ***
+             * Mettre dans ds try ou qq chosae comme ca:
+             * ***
+             **/
+            firstname = req.queryParams("firstname");
+            lastname = req.queryParams("lastname");
+            prof = req.queryParams("prof");
+            blanche = req.queryParams("blanche");
+            rouge = req.queryParams("rouge");
+            verte = req.queryParams("verte");
+
+
+            res.redirect("/users");
+            return UserGUI.create(firstname, lastname, prof, blanche, rouge, verte);
         });
 
-        get("/users/delete/:firstname/:lastname", (req, res) -> {
+        /*
+         * ***
+         * L'URL ressemblera a: localhost:8081/users/delete?firstname=julien&lastname=herbaux
+         * ***
+         **/
+        get("/users/delete", (req, res) -> {
 
             res.redirect("/users");
-            return UserGUI.delete(req.params(":firstname"), req.params(":lastname"));
+            return UserGUI.delete(req.queryParams("firstname"), req.queryParams("lastname"));
         });
     }
 }
